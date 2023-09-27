e718d3387a033a8affd77b170e1e9a9a0ccd0ceb
Argoolja <84133779+Argoolja@users.noreply.github.com>
2023-09-27 16:30:58 +0300

    Practicals 1-4

---
+++ Practical 4/src/js/script.js
@@ -0,0 +1,97 @@
+EVERY_DAY_SPENDING = 15.3
+
+
+let userName = "Andrew"
+let userAge = 21
+let userBalance = 1200
+
+
+let pets = ["Cat","Dog", "Hamster"]
+
+everyDaySpendingPerPet = 6
+daysSurvived = 0
+
+function nameVertical(name){
+    console.log(name)
+    for(i in name){
+        console.log(name[i])
+    }
+}
+
+function code(n) {
+    return (n < 100)? "Not a valid code" :
+            (n < 200)? "Informational response" :
+            (n < 300)? "Successful responses" :
+            (n < 400)? "Redirection messages" :
+            (n < 500)? "Client error response" :
+            (n < 600)? "Server error response" :
+            "Not a valid code"
+ }
+
+ function compareVariables(var1, var2) {
+    console.log("var1 type:", typeof(var1) + "; " + "var2 type:", typeof(var2))
+    if(var1 == var2 && typeof(var1) == typeof(var2)){
+        console.log("The two variables have the same value and type")
+    
+    }
+    else if (var1 == var2 && typeof(var1) != typeof(var2)){
+        console.log("The two variables have the same value but not the same type")
+    }
+    else if (var1 != var2 && typeof(var1) == typeof(var2)){
+        console.log("The two variables do not have the same value but have the same type")
+    }
+    else {
+        console.log("The two variables do not have the same value nor the same type")
+    }
+}
+
+function fibonacci(n) {
+    let out = 0
+    let onestep = 1
+    let two_steps = 0
+    let counter = 0
+    while(n >= counter){
+        out = onestep + two_steps
+        console.log(out)
+        two_steps = onestep
+        onestep = out
+        counter = counter + 1
+    }
+}
+
+
+function main(){
+    //console.log("Text", variable) allows you to write to the console
+    console.log("User Name", userName)
+    console.log("User Age", userAge)
+    console.log("User balance", userBalance)
+    console.log("User pets", pets)
+    console.log("Spending per pet", everyDaySpendingPerPet)
+    console.log("User days survived", daysSurvived)
+
+    while (userBalance > 0) {
+        let spending = EVERY_DAY_SPENDING + everyDaySpendingPerPet * pets.length
+        userBalance -= spending
+        daysSurvived++
+    }
+
+    console.log("User have sufficient money for " + daysSurvived + " days")
+
+    nameVertical("Sarah")
+    console.log(code(50))
+    console.log(code(105))
+    console.log(code(245))
+    console.log(code(312))
+    console.log(code(476))
+    console.log(code(599))
+    console.log(code(665))
+
+    let a = 1
+    let b = "1"
+
+    compareVariables(a,b)
+
+    fibonacci(10)
+}
+
+main()
