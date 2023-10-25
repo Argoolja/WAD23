547de749f76729d71f1319e6395d31752971d77e
Argoolja <84133779+Argoolja@users.noreply.github.com>
2023-10-25 15:41:05 +0300

    Practical 7 done

--- practical 7/src/js/script.js
+++ practical 7/src/js/script.js
@@ -2,13 +2,48 @@ window.onload = function() {
 
 
 
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('res/json/myjson.json')
         .then((response) => response.json())
         .then(json => {
-            console.log(json);
+
+            const divParent = document.createElement("div")
+            
+            json.forEach(element => {
+            
+            const postDiv = document.createElement("div")
+            postDiv.className = "post"
+
+            const h3 = document.createElement("h3")
+            h3.innerHTML = element.title
+            h3.className = "header"
+
+            const p = document.createElement("p")
+            p.innerHTML = element.body
+            p.className = "postContent"
+
+            postDiv.appendChild(h3)
+            postDiv.appendChild(p)
+
+            divParent.appendChild(postDiv)
+
+            });
+
+            document.body.appendChild(divParent)
 
         })
 
+        .catch(err => {
+            let errDiv = document.createElement("div");
+            errDiv.className = 'post';
+            errDiv.innerText = err;
+            document.body.appendChild(errDiv);
+        })
 
+        .finally(() => {
+            let footer = document.createElement("footer");
+            date = new Date().toLocaleString()
+            footer.innerText = date;
+            document.body.appendChild(footer);
+        })
 
 }
